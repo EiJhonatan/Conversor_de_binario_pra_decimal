fe74e991578cc0812649cdcb456bae9429d00261
Jhonatan Ferreira <devjhow2003@gmail.com>
2024-06-17 12:59:21 -0300

    adicionando o converterDecimalBinario.py

---
+++ converterDecimalBinario.py
@@ -0,0 +1,16 @@
+numeroBinario = input("Digite um numero binário pra converter pra decimal")
+
+
+def binario_para_decimal(binario):
+
+    decimal = 0
+    potencia = len(binario) - 1
+    for digito in binario:
+        decimal += int(digito) * (2 ** potencia)
+        potencia -= 1
+    
+    return decimal
+
+decimal = binario_para_decimal(numeroBinario)
+
+print(f"O número binário {numeroBinario} em decimal é {decimal}")
